90d4e2be1930950d75b75b072e384cfc53fbc4eb
JayTeeSF <jonathan@his-service.net>
2012-03-01 16:57:39 -0800

    init ...using thin via procfile

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Picks::Application.config.secret_token = '829627504630a30e943d0cbcdf43278f500a5ecc5321df2ea5cd5364cb6a9c7ba961052e9fd1c7b3198dc8911aba356b57d1a6b3df8d8b4b2f53d42ee51dc21f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Picks::Application.config.session_store :cookie_store, key: '_picks_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Picks::Application.config.session_store :active_record_store
